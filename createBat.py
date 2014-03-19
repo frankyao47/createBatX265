ad2e3675792162070f1a3069a1fbee1f1c160236
SJTUYao <yaofangjie2010@126.com>
2014-03-19 20:33:37 +0800

    4k filename format supported, param with no argu supported, suffix option added

--- createBat.py
+++ createBat.py
@@ -14,6 +14,11 @@ def addHyphen(key):
 	return len(key) == 1 and ('-' + key) or ('--' + key)
 
 def openInputFile(inputFile, isParamFile):
+	'''
+	Open the param.txt and option.txt file and grab the params.
+	The first param is the name of the file and the second shows whether its param.txt(which should be operated differently)
+	Param hyphen is added here(e.g. --preset)
+	'''
 	f = open(inputFile, 'r')
 	strsList = f.readlines()
 	paramList = [getParam(strs) for strs in strsList if getParam(strs)]
@@ -22,11 +27,16 @@ def openInputFile(inputFile, isParamFile):
 	keyList = []
 	valueList = []
 	for param in paramList:
-		key = param.split('=')[0].strip()
+		paramPair = param.split('=')
+		key = paramPair[0].strip()
 		if isParamFile:
 			key = addHyphen(key)
-		valueStrList = param.split('=')[1].split(',')
-		value = [value.strip() for value in valueStrList if value.strip()]
+
+		if len(paramPair) == 1:
+			value = []    ##if the param has no arguments, an empty list is marked
+		else:
+			valueStrList = paramPair[1].split(',')
+			value = [value.strip() for value in valueStrList if value.strip()]
 		# print valueList
 		keyList.append(key) 
 		valueList.append(value)
@@ -37,8 +47,10 @@ def openInputFile(inputFile, isParamFile):
 #####
 ######################################################################################
 def checkInputValidity(optionKeyList):
-	if not ('x265Directory' in optionKeyList and 'yuvFileDirectory' in optionKeyList and 'shutdown' in optionKeyList):
-		raise Exception('Not enough options is provided, please check!')
+	options = ['x265Directory', 'yuvFileDirectory', 'shutdown']
+	for option in options:
+		if option not in optionKeyList:
+			raise Exception('Not enough options is provided, please check!')
 
 
 ######################################################################################
@@ -46,6 +58,10 @@ def checkInputValidity(optionKeyList):
 #####
 ######################################################################################
 def getYuvFileList(path):
+	'''
+	Return all the .yuv files found from the given path.
+	Directory and single file both supported.
+	'''
 	yuvFileList = []
 	if os.path.isfile(path) and os.path.splitext(path)[1] == '.yuv':
 		yuvFileList.append(path)
@@ -67,20 +83,29 @@ def searchYuvFile(dirList):
 	for i in dirList:
 		yuvFileList.extend(getYuvFileList(i))
 
-	noDupes = []
-	[noDupes.append(i) for i in yuvFileList if not noDupes.count(i)] # remove dupes
-	return noDupes
+	fileWithNoDupes = []
+	[fileWithNoDupes.append(i) for i in yuvFileList if not fileWithNoDupes.count(i)] # remove dupes
+	return fileWithNoDupes
 
 
 ######################################################################################
 #####	write cmd logic
 #####
 ######################################################################################
-def getResFps(yuvFile):
-	filename = os.path.splitext(os.path.split(yuvFile)[1])[0]
-	(image, res, fps) = filename.split('_')
-	#print res, fps
-	return (res, fps)
+def getInfoFromFilename(yuvFileWithPath):
+	'''get information such as resolution, fps & bitdepth from a yuv filename along with its path'''
+	filename = os.path.splitext(os.path.split(yuvFileWithPath)[1])[0]
+	infoList = filename.split('_')
+	res, fps = infoList[1:3]
+
+	##The following logic should be updated if new format of naming a yuv file is applied
+	if len(infoList) == 3:
+		bitdepth = 8
+	elif infoList[3].endswith('bit'): 
+		bitdepth = int(infoList[3][:-len('bit')])  
+	else:
+		bitdepth = 8
+	return res, fps, bitdepth
 
 def cmdRecursion(f, cmd, curResultDir, outputFile, paramKeyList, paramValueList, index):
 	if index == len(paramKeyList):
@@ -97,6 +122,12 @@ def cmdRecursion(f, cmd, curResultDir, outputFile, paramKeyList, paramValueList,
 		f.write(' '.join(curCmd) + '\n')
 	else:
 		key = paramKeyList[index] #traverse list
+		if paramValueList[index] == []: ###params with no arguments
+			curOutputFile = outputFile[:]
+			curCmd = cmd[:]
+			curCmd.append('%(key)s' %locals())
+			cmdRecursion(f, curCmd, curResultDir, curOutputFile, paramKeyList, paramValueList, index+1)
+
 		for value in paramValueList[index]:
 			curOutputFile = outputFile[:]
 
@@ -108,7 +139,7 @@ def cmdRecursion(f, cmd, curResultDir, outputFile, paramKeyList, paramValueList,
 
 
 def writeSubCmd(f, resultDir, yuvFile, paramKeyList, paramValueList):
-	(res, fps) = getResFps(yuvFile)
+	(res, fps, bitdepth) = getInfoFromFilename(yuvFile)
 	filename = os.path.splitext(os.path.split(yuvFile)[1])[0]
 	curResultDir = resultDir + os.path.sep + filename
 
@@ -117,6 +148,8 @@ def writeSubCmd(f, resultDir, yuvFile, paramKeyList, paramValueList):
 	cmd.append('--input %(yuvFile)s' %locals())
 	cmd.append('--input-res %(res)s' %locals())
 	cmd.append('--fps %(fps)s' %locals())
+	if bitdepth != 8:
+		cmd.append('--input-depth %(bitdepth)s' %locals())
 	cmdRecursion(f, cmd, curResultDir, [filename.split('_')[0]], paramKeyList, paramValueList, 0)
 
 
@@ -124,7 +157,14 @@ def writeCmd(outputFile, paramKeyList, paramValueList, optionKeyList, optionValu
 	f = open(outputFile, 'w')
 	sep = os.path.sep
 	curTime = time.strftime('%Y%m%d%H%M',time.localtime(time.time()))
-	resultDir = os.getcwd() + sep + curTime
+
+	##adding test name, Chinese seems to be supported
+	if 'suffix' in optionKeyList:
+		suffix = optionValueList[optionKeyList.index('suffix')][0]
+		dirName = curTime + '_' + suffix
+	else:
+		dirName = curTime
+	resultDir = os.getcwd() + sep + dirName
 	
 	f.write('mkdir %s\n' %resultDir)
 
